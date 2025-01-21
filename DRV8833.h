fe8989dd68599bdd6fc6de82abd4ae434ca83b71
Barna Gergely <h144988@stud.u-szeged.hu>
2025-01-21 21:52:29 +0100

    Add inline docs

--- DRV8833.h
+++ DRV8833.h
@@ -9,10 +9,25 @@
 
 #include "Arduino.h"
 
-// Class definition (header) ========================================================================
+/// @brief Texas Instruments DRV8833 advanced motor driver
 class DRV8833 {
   public:
+    /// @brief DRV8833 motor driver constructor
+    /// @param pin1 The first pin connected to the motor driver. Must always be PWM capable.
+    /// @param pin2 The second pin connected to the motor driver. If the doublePWM is set to true, must also be PWM capable
+    /// @param minInput Lower limit of the controlValue (minimum of controlValue)
+    /// @param maxInput Upper limit of the controlValue (maximum of controlValue)
+    /// @param neutralWidth The width of the neutral zone where the motor does not move
+    /// @param invert Invert rotation direction
+    /// @param doublePWM Use pin2 as PWM capable
     DRV8833(int pin1, int pin2, int minInput, int maxInput, int neutralWidth, boolean invert, boolean doublePWM);
+    
+    /// @brief Drive the motor with specified speed and settings
+    /// @param controlValue Motor speed and direction
+    /// @param maxPWM Maximum PWM value limit
+    /// @param rampTime Ramp the motor speed slowly up & down to protect the gearbox. In ms per 1 PWM increment.
+    /// @param brake Activate break
+    /// @param neutralBrake Activate brake in neutral position, meaning stop the motor when the controlValue is within the neutralWidth.
     void drive(int controlValue, int maxPWM, int rampTime, boolean brake, boolean neutralBrake);
 
   private:
